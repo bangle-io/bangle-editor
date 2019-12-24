de1c5995fc4e8bb7190d7329642e614385da402c
Kushan Joshi <0o3ko0@gmail.com>
2019-12-24 16:32:23 +0530

    Improve inline command package logistics

---
+++ packages/plugins/inline-command-palette/src/helpers/with-editor-state-update.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function withEditorStateUpdate({ initialState, transformEditorState }) {
+  return (Comp) => {
+    class Wrapper extends React.PureComponent {
+      constructor(props) {
+        super(props);
+        this.state = initialState;
+
+        props.onEditorStateUpdate((tr, view, prevEditorState, editorState) => {
+          if (!editorState) {
+            return;
+          }
+
+          const newState = transformEditorState(this.state, {
+            tr,
+            view,
+            prevEditorState,
+            editorState,
+          });
+
+          if (newState !== this.state) {
+            this.setState(newState);
+          }
+        });
+      }
+
+      render() {
+        const { onEditorStateUpdate, ...otherProps } = this.props;
+        return <Comp {...otherProps} {...this.state} />;
+      }
+    }
+
+    Wrapper.propTypes = {
+      onEditorStateUpdate: PropTypes.func.isRequired,
+    };
+
+    return Wrapper;
+  };
+}
