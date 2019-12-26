23ce7ee7625fbbf49892ada8b7a1ba33b5c80298
Kushan Joshi <0o3ko0@gmail.com>
2019-12-26 16:20:15 +0530

    Chrome selection bug

--- packages/bangle-utils/src/index.js
+++ packages/bangle-utils/src/index.js
@@ -2,5 +2,13 @@ import * as nodeHelpers from './node-helpers';
 import * as logging from './logging';
 import { reactNodeViewHOC, ReactNodeView } from './react-node-view';
 import * as menuPlugin from './menu-plugin';
-
-export { nodeHelpers, logging, reactNodeViewHOC, ReactNodeView, menuPlugin };
+import browser, { isChromeWithSelectionBug } from './browser';
+export {
+  nodeHelpers,
+  logging,
+  reactNodeViewHOC,
+  ReactNodeView,
+  menuPlugin,
+  browser,
+  isChromeWithSelectionBug,
+};
