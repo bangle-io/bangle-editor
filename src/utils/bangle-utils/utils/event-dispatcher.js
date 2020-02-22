001c1f74fd224c46068c1bab2a1f32fd056fd463
Kushan Joshi <0o3ko0@gmail.com>
2020-02-21 21:58:10 -0800

    Improving portal's rendering performance by making the rendering selective.

---
+++ src/utils/bangle-utils/utils/event-dispatcher.js
@@ -0,0 +1,28 @@
+export class EventDispatcher {
+  constructor() {
+    this.listeners = {};
+  }
+  on(event, cb) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = new Set();
+    }
+    this.listeners[event].add(cb);
+  }
+  off(event, cb) {
+    if (!this.listeners[event]) {
+      return;
+    }
+    if (this.listeners[event].has(cb)) {
+      this.listeners[event].delete(cb);
+    }
+  }
+  emit(event, data) {
+    if (!this.listeners[event]) {
+      return;
+    }
+    this.listeners[event].forEach((cb) => cb(data));
+  }
+  destroy() {
+    this.listeners = {};
+  }
+}
