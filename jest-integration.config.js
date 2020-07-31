2a1c0a634016806e3c37610fe97edc96b2029ab9
Kushan Joshi <0o3ko0@gmail.com>
2020-07-30 22:40:11 -0700

    Cleanup and postcss command fix

--- jest-integration.config.js
+++ jest-integration.config.js
@@ -4,8 +4,5 @@ module.exports = {
   preset: 'jest-puppeteer',
   testRunner: 'jest-circus/runner',
   modulePaths: ['<rootDir>/src'],
-  testMatch: [
-    // '**/tests-integration/**/*.[jt]s?(x)',
-    '**/tests-integration/**/?(*.)+(spec|test).[jt]s?(x)',
-  ],
+  testMatch: ['**/tests-integration/**/?(*.)+(spec|test).[jt]s?(x)'],
 };
