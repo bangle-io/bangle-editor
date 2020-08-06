cfa42fe25037800d5d4bb6b072351f45d511e173
Kushan Joshi <0o3ko0@gmail.com>
2020-08-06 00:28:58 -0700

    Yet another Portal  tweak (#4)
    * simplify portal.js
    
    * minor: disable custom-node-view logging
    
    * minor: add render key to displayName of SelectiveUpdate
    
    * Move out destroyNodeView renderNodeView to parent component
    
    * Clean up react-editor
    
    * Minor renaming
    
    * Move portal management to its own component

--- src/utils/bangle-utils/helper-react/portal.js
+++ src/utils/bangle-utils/helper-react/portal.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { createPortal } from 'react-dom';
+import React from 'react';
+import reactDOM from 'react-dom';
 import { objUid } from '../utils/object-uid';
 import { Emitter } from '../utils/emitter';
 import { CachedMap } from '../utils/js-utils';
+
 const LOG = true;
 
 function log(...args) {
@@ -16,49 +17,36 @@ export class PortalProviderAPI extends Emitter {
     return this.#portalsMap.arrayValues();
   }
 
-  render(Element, props, container) {
-    const renderKey = objUid.get(container);
+  // Returns true if a new element was added
+  // false if an element already existed
+  render({ dom, extension, renderingPayload }) {
+    const renderKey = objUid.get(dom);
     // If the element already exists communicate
     // to selectively update it, bypassing the entire array re-render in PortalRenderer
-    if (this.#portalsMap.has(container)) {
+    if (this.#portalsMap.has(dom)) {
       log('PortalProviderAPI: updating existing', renderKey);
-      this.emit(renderKey, props);
-      return;
+      this.emit(renderKey, renderingPayload);
+      return false;
     }
 
     log('PortalProviderAPI: creating new', renderKey);
 
-    const SelectiveComp = React.memo(
-      (props) => {
-        const [state, setState] = useState(props);
-        const emitter = this;
-        const renderKey = objUid.get(container);
+    if (!extension.render.displayName) {
+      extension.render.displayName = `ParentNodeView[${extension.name}]`;
+    }
 
-        React.useEffect(() => {
-          log('setting up', renderKey);
-          emitter.on(renderKey, setState);
-          return () => {
-            log('emitter.off', renderKey);
-            emitter.off(renderKey, setState);
-          };
-        }, [emitter, renderKey]);
-        log('rendering func', renderKey);
-
-        return <Element {...state} />;
-      },
-      // We never want to update the function via props
-      () => true,
-    );
-
-    const portalElement = createPortal(
-      <SelectiveComp {...props} />,
-      container,
+    const portalElement = createPortal({
+      Element: extension.render,
+      emitter: this,
       renderKey,
-    );
+      childProps: renderingPayload,
+      forceUpdateKey: '#force_update',
+      dom: dom,
+    });
 
-    log('adding', objUid.get(container));
-    this.#portalsMap.set(container, portalElement);
-    this.emit('#root_update');
+    log('adding', renderKey);
+    this.#portalsMap.set(dom, portalElement);
+    return true;
   }
 
   forceUpdate() {
@@ -66,10 +54,11 @@ export class PortalProviderAPI extends Emitter {
     this.emit('#force_update');
   }
 
-  remove(container) {
-    log('removing', objUid.get(container));
-    this.#portalsMap.delete(container);
-    this.emit('#root_update');
+  remove(dom) {
+    const renderKey = objUid.get(dom);
+    log('removing', renderKey);
+    this.#portalsMap.delete(dom);
+    return true;
   }
 
   destroy() {
@@ -77,4 +66,70 @@ export class PortalProviderAPI extends Emitter {
     this.#portalsMap = null;
     super.destroy();
   }
+}
+
+function createPortal({
+  Element,
+  emitter,
+  renderKey,
+  childProps,
+  forceUpdateKey,
+  dom,
+}) {
+  class SelectiveUpdate extends React.Component {
+    static displayName = `SelectiveUpdate_${renderKey}[${Element.displayName}]`;
+
+    static getDerivedStateFromError(error) {
+      return { hasError: true, childProps: null };
+    }
+
+    state = {
+      hasError: false,
+      childProps: childProps,
+    };
+
+    shouldComponentUpdate(nextProps, nextState) {
+      // We never want to update the function via props
+      // instead it will be updated via the emitter
+      if (this.state.hasError !== nextState.hasError) {
+        return true;
+      }
+      return this.state.childProps !== nextState.childProps;
+    }
+
+    componentDidCatch(error, errorInfo) {
+      console.log(errorInfo);
+      console.error(error);
+    }
+
+    componentDidMount() {
+      emitter.on(renderKey, this.handleUpdate);
+      emitter.on(forceUpdateKey, this.forceUpdate);
+    }
+
+    componentWillUnmount() {
+      emitter.off(renderKey, this.handleUpdate);
+      emitter.off(forceUpdateKey, this.forceUpdate);
+    }
+
+    handleUpdate = (childProps) => {
+      this.setState({
+        childProps,
+      });
+    };
+
+    render() {
+      if (this.state.hasError) {
+        return (
+          <div style={{ backgroundColor: 'red', color: 'white' }}>
+            Error in {Element.displayName}
+          </div>
+        );
+      }
+      log('rendering:', renderKey);
+      return <Element {...this.state.childProps} />;
+    }
+  }
+
+  return reactDOM.createPortal(<SelectiveUpdate />, dom, renderKey);
 }
