8bcd421266f80a6810782629ec9b85b855a97d9b
Kushan Joshi <0o3ko0@gmail.com>
2020-08-20 18:08:29 -0700

    Add paste event

---
+++ src/test-helpers/create-event.js
@@ -0,0 +1,39 @@
+/**
+ */
+const supportsEvent = ((event) => {
+  if (event) {
+    try {
+      // eslint-disable-next-line no-unused-expressions
+      new event('emit-init');
+    } catch (e) {
+      return false;
+    }
+  }
+  return true;
+})(Event);
+
+/**
+ * Build an event object in a cross-browser manner
+ *
+ * Usage:
+ *    const event = createEvent('paste', options);
+ */
+export default (name, options = {}) => {
+  let event;
+  if (options.bubbles === undefined) {
+    options.bubbles = true;
+  }
+  if (options.cancelable === undefined) {
+    options.cancelable = true;
+  }
+  if (options.composed === undefined) {
+    options.composed = true;
+  }
+  if (supportsEvent) {
+    event = new Event(name, options);
+  } else {
+    event = document.createEvent('Event');
+    event.initEvent(name, options.bubbles, options.cancelable);
+  }
+  return event;
+};
