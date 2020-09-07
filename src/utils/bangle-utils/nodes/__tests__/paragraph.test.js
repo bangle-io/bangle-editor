ebeb39640ae25270d9b5be0a39242ce73c0c23ff
Kushan Joshi <0o3ko0@gmail.com>
2020-09-07 16:21:14 -0700

    move paragraph to psx

--- src/utils/bangle-utils/nodes/__tests__/paragraph.test.js
+++ src/utils/bangle-utils/nodes/__tests__/paragraph.test.js
@@ -5,21 +5,7 @@
 import { psx } from '../../../../test-helpers/schema-builders';
 import '../../../../../src/test-helpers/jest-helpers';
 
-import {
-  doc,
-  ul,
-  li,
-  p,
-  ol,
-  br,
-  h1,
-  codeBlock,
-  underline,
-} from '../../../../../src/test-helpers/test-builders';
 import { renderTestEditor } from '../../../../test-helpers/render-helper';
-
-import { applyCommand } from '../../../../../src/test-helpers/commands-helpers';
-
 import { OrderedList } from '../ordered-list';
 import { BulletList } from '../bullet-list';
 import { ListItem } from '../list-item/list-item';
@@ -28,17 +14,10 @@ import {
   typeText,
 } from '../../../../../src/test-helpers/keyboard';
 import { Underline } from '../../../../../src/utils/bangle-utils/marks';
-
 import { CodeBlock } from '../code-block';
 import { Heading } from '../heading';
 import { HardBreak } from '../hard-break';
 
-import {
-  enterKeyCommand,
-  splitListItem,
-  toggleList,
-  backspaceKeyCommand,
-} from '../list-item/commands';
 import { Blockquote } from '../blockquote';
 import { TodoList } from '../todo-list';
 import { TodoItem } from '../todo-item';
@@ -59,8 +38,8 @@ const extensions = [
 
 const testEditor = renderTestEditor({ extensions, type: 'new' });
 
-describe.only('Basics', () => {
-  test.only('is able to type paragraph', async () => {
+describe('Basics', () => {
+  test('is able to type paragraph', async () => {
     const { editor } = await testEditor(
       <doc>
         <para>foo[]bar</para>
@@ -79,89 +58,183 @@ describe.only('Basics', () => {
   test('is able to create a new paragraph', async () => {
     const { editor } = await testEditor(
       <doc>
-        <p>foo[]bar</p>
+        <para>foo[]bar</para>
       </doc>,
     );
 
     sendKeyToPm(editor.view, 'Enter');
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('foo'), p('{<>}bar')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>foo</para>
+        <para>[]bar</para>
+      </doc>,
+    );
   });
 
   test('is able to create a new paragraph', async () => {
-    const { editor } = await testEditor(doc(p('foobar{<>}')));
+    const { editor } = await testEditor(
+      <doc>
+        <para>foobar[]</para>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, 'Enter');
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('foobar'), p('{<>}')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>foobar</para>
+        <para>[]</para>
+      </doc>,
+    );
   });
 
   // TODO this is broken for some reason
   test.skip('is able to backspace', async () => {
-    const { editor } = await testEditor(doc(p('foobar{<>}')));
+    const { editor } = await testEditor(
+      <doc>
+        <para>foobar[]</para>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, 'Backspace');
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('fooba{<>}')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>fooba[]</para>
+      </doc>,
+    );
   });
 });
 
 describe('Moving selection start and end', () => {
   const { keys } = new Paragraph().options;
   it('Moves selection to the start', async () => {
-    const { editor } = await testEditor(doc(p('foobar{<>}')));
+    const { editor } = await testEditor(
+      <doc>
+        <para>foobar[]</para>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, keys.jumpToStartOfLine);
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('{<>}foobar')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>[]foobar</para>
+      </doc>,
+    );
   });
 
   it('Moves selection to the start', async () => {
-    const { editor } = await testEditor(doc(p('f{<>}oobar')));
+    const { editor } = await testEditor(
+      <doc>
+        <para>f[]oobar</para>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, keys.jumpToStartOfLine);
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('{<>}foobar')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>[]foobar</para>
+      </doc>,
+    );
   });
 
   it('Moves selection to the start', async () => {
-    const { editor } = await testEditor(doc(p('{<>}foobar')));
+    const { editor } = await testEditor(
+      <doc>
+        <para>[]foobar</para>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, keys.jumpToStartOfLine);
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('{<>}foobar')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>[]foobar</para>
+      </doc>,
+    );
   });
 
   it('Moves selection to start when inside a list', async () => {
-    const { editor } = await testEditor(doc(ul(li(p('foobar{<>}')))));
+    const { editor } = await testEditor(
+      <doc>
+        <ul>
+          <li>
+            <para>foobar[]</para>
+          </li>
+        </ul>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, keys.jumpToStartOfLine);
 
-    expect(editor.state).toEqualDocAndSelection(doc(ul(li(p('{<>}foobar')))));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <ul>
+          <li>
+            <para>[]foobar</para>
+          </li>
+        </ul>
+      </doc>,
+    );
   });
 
   it('Moves selection to the end', async () => {
-    const { editor } = await testEditor(doc(p('{<>}foobar')));
+    const { editor } = await testEditor(
+      <doc>
+        <para>[]foobar</para>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, keys.jumpToEndOfLine);
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('foobar{<>}')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>foobar[]</para>
+      </doc>,
+    );
   });
 
   it('Moves selection to the end', async () => {
-    const { editor } = await testEditor(doc(p('fooba{<>}r')));
+    const { editor } = await testEditor(
+      <doc>
+        <para>fooba[]r</para>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, keys.jumpToEndOfLine);
 
-    expect(editor.state).toEqualDocAndSelection(doc(p('foobar{<>}')));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <para>foobar[]</para>
+      </doc>,
+    );
   });
 
   it('Moves selection to end when inside a list', async () => {
-    const { editor } = await testEditor(doc(ul(li(p('{<>}foobar')))));
+    const { editor } = await testEditor(
+      <doc>
+        <ul>
+          <li>
+            <para>[]foobar</para>
+          </li>
+        </ul>
+      </doc>,
+    );
 
     sendKeyToPm(editor.view, keys.jumpToEndOfLine);
 
-    expect(editor.state).toEqualDocAndSelection(doc(ul(li(p('foobar{<>}')))));
+    expect(editor.state).toEqualDocAndSelection(
+      <doc>
+        <ul>
+          <li>
+            <para>foobar[]</para>
+          </li>
+        </ul>
+      </doc>,
+    );
   });
 });
 
@@ -179,153 +252,272 @@ describe('Moving up and down', () => {
 
   it('basic', async () => {
     await check(
-      doc(p('foobar'), p('hello{<>}')),
-      doc(p('hello{<>}'), p('foobar')),
+      <doc>
+        <para>foobar</para>
+        <para>hello[]</para>
+      </doc>,
+      <doc>
+        <para>hello[]</para>
+        <para>foobar</para>
+      </doc>,
     );
   });
 
   it('works with underline ', async () => {
     await check(
-      doc(p('foobar'), p('hel', underline('lo{<>}'))),
-      doc(p('hel', underline('lo{<>}')), p('foobar')),
+      <doc>
+        <para>foobar</para>
+        <para>
+          he<underline>lo[]</underline>
+        </para>
+      </doc>,
+      <doc>
+        <para>
+          he<underline>lo[]</underline>
+        </para>
+        <para>foobar</para>
+      </doc>,
     );
   });
 
   it('works with hard break', async () => {
     await check(
-      doc(p('foobar'), p('hello{<>}', br(), 'why')),
-      doc(p('hello{<>}', br(), 'why'), p('foobar')),
+      <doc>
+        <para>foobar</para>
+        <para>
+          hello[]
+          <br />
+          why
+        </para>
+      </doc>,
+      <doc>
+        <para>
+          hello[]
+          <br />
+          why
+        </para>
+        <para>foobar</para>
+      </doc>,
     );
   });
 
   it('works with hard break', async () => {
     await check(
-      doc(p('foobar'), p('hello', br(), '{<>}why')),
-      doc(p('hello', br(), '{<>}why'), p('foobar')),
+      <doc>
+        <para>foobar</para>
+        <para>
+          hello
+          <br />
+          []why
+        </para>
+      </doc>,
+      <doc>
+        <para>
+          hello
+          <br />
+          []why
+        </para>
+        <para>foobar</para>
+      </doc>,
     );
   });
 
   it('swaps with ul list', async () => {
-    // prettier-ignore
     await check(
-      doc(
-        p('foobar'), 
-        ul(li(p('hello', br())), li(p('why'))),
-        p('{<>}lorem'),
-      ),
-      doc(
-        p('foobar'), 
-        p('{<>}lorem'),
-        ul(li(p('hello', br())), li(p('why'))),
-      ),
+      <doc>
+        <para>foobar</para>
+        <ul>
+          <li>
+            <para>
+              hello
+              <br />
+            </para>
+          </li>
+          <li>
+            <para>why</para>
+          </li>
+        </ul>
+        <para>[]lorem</para>
+      </doc>,
+      <doc>
+        <para>foobar</para>
+        <para>[]lorem</para>
+        <ul>
+          <li>
+            <para>
+              hello
+              <br />
+            </para>
+          </li>
+          <li>
+            <para>why</para>
+          </li>
+        </ul>
+      </doc>,
     );
   });
 
   it('swaps with ol list', async () => {
-    // prettier-ignore
     await check(
-      doc(
-        p('foobar'), 
-        ol(li(p('hello', br())), li(p('why'))),
-        p('{<>}lorem'),
-      ),
-      doc(
-        p('foobar'), 
-        p('{<>}lorem'),
-        ol(li(p('hello', br())), li(p('why'))),
-      ),
+      <doc>
+        <para>foobar</para>
+        <ol>
+          <li>
+            <para>
+              hello
+              <br />
+            </para>
+          </li>
+          <li>
+            <para>why</para>
+          </li>
+        </ol>
+        <para>[]lorem</para>
+      </doc>,
+      <doc>
+        <para>foobar</para>
+        <para>[]lorem</para>
+        <ol>
+          <li>
+            <para>
+              hello
+              <br />
+            </para>
+          </li>
+          <li>
+            <para>why</para>
+          </li>
+        </ol>
+      </doc>,
     );
   });
 
   it('swaps with ol list', async () => {
-    // prettier-ignore
     await check(
-      doc(
-        p('foobar'), 
-        ol(li(p('hello', br())), li(p('why'))),
-        p('{<>}lorem'),
-      ),
-      doc(
-        p('foobar'), 
-        p('{<>}lorem'),
-        ol(li(p('hello', br())), li(p('why'))),
-      ),
+      <doc>
+        <para>foobar</para>
+        <ol>
+          <li>
+            <para>
+              hello
+              <br />
+            </para>
+          </li>
+          <li>
+            <para>why</para>
+          </li>
+        </ol>
+        <para>[]lorem</para>
+      </doc>,
+      <doc>
+        <para>foobar</para>
+        <para>[]lorem</para>
+        <ol>
+          <li>
+            <para>
+              hello
+              <br />
+            </para>
+          </li>
+          <li>
+            <para>why</para>
+          </li>
+        </ol>
+      </doc>,
     );
   });
 
   it('swaps with heading', async () => {
-    // prettier-ignore
     await check(
-      doc(
-        p('foobar'), 
-        h1('hi'),
-        p('{<>}lorem'),
-      ),
-      doc(
-        p('foobar'), 
-        p('{<>}lorem'),
-        h1('hi'),
-      ),
+      <doc>
+        <para>foobar</para>
+        <heading level="1">hi</heading>
+        <para>[]lorem</para>
+      </doc>,
+      <doc>
+        <para>foobar</para>
+        <para>[]lorem</para>
+        <heading level="1">hi</heading>
+      </doc>,
     );
   });
 
   it('swaps with codeBlock', async () => {
-    // prettier-ignore
     await check(
-      doc(
-        p('foobar'), 
-        codeBlock()('hi'),
-        p('{<>}lorem'),
-      ),
-      doc(
-        p('foobar'), 
-        p('{<>}lorem'),
-        codeBlock()('hi'),
-      ),
+      <doc>
+        <para>foobar</para>
+        <codeBlock>hi</codeBlock>
+        <para>[]lorem</para>
+      </doc>,
+      <doc>
+        <para>foobar</para>
+        <para>[]lorem</para>
+        <codeBlock>hi</codeBlock>
+      </doc>,
     );
   });
 });
 
 describe('Insert empty paragraph above and below', () => {
-  test.each(
-    // prettier-ignore
+  test.each([
     [
-      [
-        <doc><p>foo[]bar</p></doc>, 
-        doc(p('{<>}'), p('foobar'))
-      ],
-      [
-        doc(p('{<>}foobar')), 
-        doc(p('{<>}'), p('foobar'))
-      ],
-      [
-        doc(p('{<>}')), 
-        doc(p('{<>}'), p())
-      ],
-      [
-        doc(
-          p('other paragraph'),
-          p('hello{<>}')
-        ), 
-        doc(
-          p('other paragraph'),
-          p('{<>}'), 
-          p('hello'),
-        )
-      ],
-      [
-        doc(
-          ul(li(p('top'))),
-          p('hello{<>}'),
-        ), 
-        doc(
-          ul(li(p('top'))),
-          p('{<>}'), 
-          p('hello')
-        )
-      ]
+      <doc>
+        <para>foo[]bar</para>
+      </doc>,
+      <doc>
+        <para>[]</para>
+        <para>foobar</para>
+      </doc>,
     ],
-  )('Case %# insert above', async (input, expected) => {
+    [
+      <doc>
+        <para>[]foobar</para>
+      </doc>,
+      <doc>
+        <para>[]</para>
+        <para>foobar</para>
+      </doc>,
+    ],
+    [
+      <doc>
+        <para>[]</para>
+      </doc>,
+      <doc>
+        <para>[]</para>
+        <para></para>
+      </doc>,
+    ],
+    [
+      <doc>
+        <para>other paragraph</para>
+        <para>hello[]</para>
+      </doc>,
+      <doc>
+        <para>other paragraph</para>
+        <para>[]</para>
+        <para>hello</para>
+      </doc>,
+    ],
+    [
+      <doc>
+        <ul>
+          <li>
+            <para>top</para>
+          </li>
+        </ul>
+        <para>hello[]</para>
+      </doc>,
+      <doc>
+        <ul>
+          <li>
+            <para>top</para>
+          </li>
+        </ul>
+        <para>[]</para>
+        <para>hello</para>
+      </doc>,
+    ],
+  ])('Case %# insert above', async (input, expected) => {
     const { editor } = await testEditor(input);
 
     sendKeyToPm(editor.view, 'Cmd-Shift-Enter');
@@ -333,45 +525,65 @@ describe('Insert empty paragraph above and below', () => {
     expect(editor.state).toEqualDocAndSelection(expected);
   });
 
-  test.each(
-    // prettier-ignore
+  test.each([
     [
-      [
-        <doc><p>foo[]bar</p></doc>, 
-        doc(p('foobar'), p('{<>}'))
-      ],
-      [
-        doc(p('{<>}foobar')), 
-        doc(p('foobar'), p('{<>}'))
-      ],
-      [
-        doc(p('{<>}')), 
-        doc(p(), p('{<>}'))
-      ],
-      [
-        doc(
-          p('other paragraph'),
-          p('hello{<>}')
-        ), 
-        doc(
-          p('other paragraph'),
-          p('hello'),
-          p('{<>}'), 
-        )
-      ],
-      [
-        doc(
-          ul(li(p('top'))),
-          p('hello{<>}'),
-        ), 
-        doc(
-          ul(li(p('top'))),
-          p('hello'),
-          p('{<>}'), 
-        )
-      ]
+      <doc>
+        <para>foo[]bar</para>
+      </doc>,
+      <doc>
+        <para>foobar</para>
+        <para>[]</para>
+      </doc>,
     ],
-  )('Case %# insert below', async (input, expected) => {
+    [
+      <doc>
+        <para>[]foobar</para>
+      </doc>,
+      <doc>
+        <para>foobar</para>
+        <para>[]</para>
+      </doc>,
+    ],
+    [
+      <doc>
+        <para>[]</para>
+      </doc>,
+      <doc>
+        <para></para>
+        <para>[]</para>
+      </doc>,
+    ],
+    [
+      <doc>
+        <para>other paragraph</para>
+        <para>hello[]</para>
+      </doc>,
+      <doc>
+        <para>other paragraph</para>
+        <para>hello</para>
+        <para>[]</para>
+      </doc>,
+    ],
+    [
+      <doc>
+        <ul>
+          <li>
+            <para>top</para>
+          </li>
+        </ul>
+        <para>hello[]</para>
+      </doc>,
+      <doc>
+        <ul>
+          <li>
+            <para>top</para>
+          </li>
+        </ul>
+        <para>hello</para>
+        <para>[]</para>
+      </doc>,
+    ],
+  ])('Case %# insert below', async (input, expected) => {
     const { editor } = await testEditor(input);
 
     sendKeyToPm(editor.view, 'Cmd-Enter');
