b2321945b331ec73ed383641e076580d504dff9a
Kushan Joshi <0o3ko0@gmail.com>
2020-10-17 14:18:44 -0700

    remove nativefs

--- bangle-play/app/components/native-file-api/errors.js
+++
@@ -1,9 +0,0 @@
-export class NoPermissionError extends Error {
-  constructor(errorCode, body) {
-    super(body);
-    this.errorCode = errorCode;
-    this.body = body;
-    Error.captureStackTrace(this, NoPermissionError);
-    this.name = this.constructor.name;
-  }
-}
