69a3af330ab8a0b68a4ab2252565380cd65dab00
Kushan Joshi <0o3ko0@gmail.com>
2020-11-08 19:46:58 -0800

    fix emoji tests

--- bangle-core/utils/js-utils.js
+++ bangle-core/utils/js-utils.js
@@ -420,3 +420,20 @@ export function recursiveFlat(data, callbackPayload) {
 
   return recurse(data);
 }
+
+export function createElement(type = 'div', attrs = {}) {
+  const element = document.createElement(type);
+  for (let attr in attrs) {
+    const value = attrs[attr];
+    if (attr.startsWith('data-')) {
+      element.setAttribute(attr, value);
+    } else if (attr === 'id') {
+      element.id = value;
+    } else if (attr === 'class') {
+      value.split(' ').forEach((c) => element.classList.add(c));
+    } else {
+      throw new Error(`Attr ${attr} not supported`);
+    }
+  }
+  return element;
+}
