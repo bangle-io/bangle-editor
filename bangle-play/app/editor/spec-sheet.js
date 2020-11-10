2bca7a79016a4ae1c4dbe193e86dbdaa1aee9f76
Kushan Joshi <0o3ko0@gmail.com>
2020-11-10 02:41:17 +0000

    make changes to bangle-play

--- bangle-play/app/editor/spec-sheet.js
+++ bangle-play/app/editor/spec-sheet.js
@@ -1,24 +1,23 @@
+import 'bangle-core/style.css';
 import { coreSpec } from 'bangle-core/components';
 import * as collab from 'bangle-plugins/collab/client/collab-extension';
-// import { dinos } from 'bangle-plugins/dinos/index';
+// import { dino } from 'bangle-plugins/dino/index';
 import { emoji, emojiInlineSuggest } from 'bangle-plugins/emoji/index';
 import 'bangle-plugins/emoji/emoji.css';
 
 import 'bangle-plugins/inline-menu/inline-menu.css';
 import * as floatingMenu from 'bangle-plugins/inline-menu/floating-menu';
 import * as linkMenu from 'bangle-plugins/inline-menu/link-menu';
-import { stopwatch } from 'bangle-plugins/stopwatch/index';
 
 import { config } from './config';
 import { trailingNode } from 'bangle-plugins/trailing-node/index';
 import { timestamp } from 'bangle-plugins/timestamp/index';
 import { SpecSheet } from 'bangle-core/spec-sheet';
-import { dinos } from 'bangle-react/components/index';
+import { dino, stopwatch } from 'bangle-react/components/index';
 
 export const specSheet = new SpecSheet([
   ...coreSpec({ heading: { levels: config.headingLevels } }),
   collab.spec(),
-  // dinos.spec(),
   emoji.spec(),
   floatingMenu.spec(),
   linkMenu.spec(),
@@ -26,5 +25,5 @@ export const specSheet = new SpecSheet([
   stopwatch.spec(),
   trailingNode.spec(),
   timestamp.spec(),
-  dinos.spec(),
+  dino.spec(),
 ]);
