7044891d02dd834b7da38e3ddf5fdba38f64915c
Kushan Joshi <0o3ko0@gmail.com>
2020-11-10 02:40:44 +0000

    fix todo list

--- bangle-core/__tests__/node-view-helpers.test.js
+++ bangle-core/__tests__/node-view-helpers.test.js
@@ -1,6 +1,6 @@
 import { paragraph, doc, text } from 'bangle-core/index';
 import { serializationHelpers } from 'bangle-core/node-view';
-import { SpecSheet } from '../../spec-sheet';
+import { SpecSheet } from '../spec-sheet';
 
 describe('serializationHelpers parseDOM', () => {
   test('does not get affected by other attributes', () => {
