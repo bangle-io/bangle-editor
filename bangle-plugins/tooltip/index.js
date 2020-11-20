e65058e3eb00556637648b3fb4bb18dd1f41b2f7
Kushan Joshi <0o3ko0@gmail.com>
2020-11-20 03:02:53 +0000

    suggest tooltip cleanup

--- bangle-plugins/tooltip/index.js
+++ bangle-plugins/tooltip/index.js
@@ -1,4 +1,4 @@
 export * from './create-tooltip-dom';
 export * as tooltipPlacement from './tooltip-placement';
 export * as selectionTooltip from './selection-tooltip';
-export * as suggestionsTooltip from './suggestions-tooltip';
+export * as suggestTooltip from './suggest-tooltip';
