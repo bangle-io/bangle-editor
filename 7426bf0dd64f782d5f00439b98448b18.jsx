9bb89999382570b4e72e8cffcd0b7af162cd06da
kepta <kepta@users.noreply.github.com>
2020-12-09 06:51:31 +0000

    deploy: aa9e2df7b12470c07b34629139daf8b521021837

--- 7426bf0dd64f782d5f00439b98448b18.jsx
+++ 7426bf0dd64f782d5f00439b98448b18.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEditorState, ReactEditorView } from '@banglejs/react';
+import { useEditorState, BangleEditorView } from '@banglejs/react';
 
 import '@banglejs/core/style.css';
 
@@ -7,5 +7,5 @@ export default function Editor() {
   const editorState = useEditorState({
     initialValue: 'Hello world!',
   });
-  return <ReactEditorView editorState={editorState} />;
+  return <BangleEditorView state={editorState} />;
 }
