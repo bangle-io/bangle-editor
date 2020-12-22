0a2cc337b743eb12c14fea21b0e20928dfbd1939
kepta <kepta@users.noreply.github.com>
2020-12-22 02:06:26 +0000

    deploy: 9b9eb93f6ec966d7dc80df64d5464c45fbae97b1

---
+++ 58e387d40565614ddb3e497900562be6.jsx
@@ -0,0 +1,22 @@
+import { subscript } from '@bangle.dev/text-formatting';
+import '@bangle.dev/core/style.css';
+import { BangleEditor, BangleEditorState } from '@bangle.dev/core';
+import { setSelectionAtEnd } from '@bangle.dev/core/core-commands';
+
+export default function Editor(domNode) {
+  const state = new BangleEditorState({
+    specs: [subscript.spec()],
+    plugins: [
+      subscript.plugins({
+        keybindings: { toggleSuperscript: 'Ctrl-s' },
+      }),
+    ],
+    initialValue: `<p>Hello there, let us see some <sup>subscript</sup>!</p>`,
+  });
+
+  const editor = new BangleEditor(domNode, { state });
+  const { view } = editor;
+  // set selection at end to demo the example
+  setSelectionAtEnd(view.state.doc)(view.state, view.dispatch);
+  return editor;
+}
