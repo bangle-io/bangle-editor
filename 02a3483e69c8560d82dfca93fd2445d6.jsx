4324a098940a623d9d15a394a04fb8cc647b98e6
kepta <kepta@users.noreply.github.com>
2021-03-04 10:30:54 +0000

    deploy: 3a074f539afc711aff99ecfc064469e066a338bb

---
+++ 02a3483e69c8560d82dfca93fd2445d6.jsx
@@ -0,0 +1,29 @@
+import '@bangle.dev/core/style.css';
+
+import { BangleEditor, BangleEditorState } from '@bangle.dev/core';
+import { listItem, bulletList, orderedList } from '@bangle.dev/core';
+
+export default function Editor(domNode) {
+  const state = new BangleEditorState({
+    specs: [
+      listItem.spec(),
+      orderedList.spec(),
+      bulletList.spec(),
+      // The above list specs are needed
+      // for todo lists to work
+    ],
+    plugins: [listItem.plugins(), orderedList.plugins(), bulletList.plugins()],
+    initialValue: `<div>
+    <p>Let us check off some lists:</p>
+    <ul>
+      <li data-bangle-name="listItem" data-bangle-attrs="{&quot;todoChecked&quot;:false}"><p>Buy milk</p></li>
+      <li data-bangle-name="listItem" data-bangle-attrs="{&quot;todoChecked&quot;:false}"><p>Clean the room</p></li>
+      <li data-bangle-name="listItem" data-bangle-attrs="{&quot;todoChecked&quot;:false}"><p>Star bangle.dev on Github</p></li>
+    </ul>
+    </div>`,
+  });
+
+  const editor = new BangleEditor(domNode, { state });
+
+  return editor;
+}
