a4de3baec2b64e684d9fa866e2686c4796f7fc9c
Lucy Wang <wxitb2017@gmail.com>
2021-05-13 21:07:08 +0800

    handle Error.captureStackTrace not available on Safari

--- collab/collab-error.js
+++ collab/collab-error.js
@@ -3,7 +3,16 @@ export class CollabError extends Error {
     super(body);
     this.errorCode = errorCode;
     this.body = body;
-    Error.captureStackTrace(this, CollabError);
+    // Error.captureStackTrace is a v8-specific method so not avilable on
+    // Firefox or Safari
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, CollabError);
+    } else {
+      const stack = new Error().stack;
+      if (stack) {
+        this.stack = stack;
+      }
+    }
     this.name = this.constructor.name;
   }
 }
