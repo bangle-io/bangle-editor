353050cd79eed9b4c69596a89997fb0d03626e29
Kushan Joshi <0o3ko0@gmail.com>
2021-05-25 19:17:14 -0700

    typescript setup

---
+++ collaboration/__tests__/special.ts
@@ -0,0 +1,4 @@
+import { sum } from '../index';
+/* eslint-disable no-process-env */
+const v = process.env.NODE_ENV;
+console.log({ v });
---
+++ collaboration/index.ts
@@ -0,0 +1,6 @@
+export function foo() {}
+
+export function sum(a: number, b: number) {
+  console.log('hix');
+  return a + b;
+}
