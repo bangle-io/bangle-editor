cf0fcf7a15a95916a3ed765c97cbeca76af56335
Kushan Joshi <0o3ko0@gmail.com>
2021-07-11 23:08:19 -0700

    add note on objectUid

--- contrib/js-utils/object-uid.ts
+++ contrib/js-utils/object-uid.ts
@@ -1,4 +1,6 @@
 let counter = 0;
+// add a unique string to prevent clashes in case multiple objectUid instances exist
+// this can happen for example due to duplicate npm packages due to version or dependency mismatch
 let unique = Math.random();
 
 export class ObjectUID extends WeakMap {
