33f02f65aaed7856c99d376ef7993261bc18834a
Kushan Joshi <0o3ko0@gmail.com>
2022-06-11 17:54:55 -0400

    fix type issues

--- components/react-emoji-suggest/src/index.ts
+++ components/react-emoji-suggest/src/index.ts
@@ -1,3 +1,7 @@
 import * as emojiSuggest from './emoji-suggest';
 export * from './EmojiSuggest';
 export { emojiSuggest };
+
+export type { GetEmojiGroupsType } from './emoji-suggest';
+export { selectEmoji } from './emoji-suggest';
+export { getSquareDimensions, resolveCounter, resolveRowJump } from './utils';
--- lib/pm/src/view.ts
+++ lib/pm/src/view.ts
@@ -1,12 +1,13 @@
-export {
+import type { ResolvedPos, Slice } from 'prosemirror-model';
+import type { EditorView } from 'prosemirror-view';
+import {
   // @ts-expect-error
   __parseFromClipboard,
   // @ts-expect-error
   __serializeForClipboard,
-  Decoration,
-  DecorationSet,
-  EditorView,
 } from 'prosemirror-view';
+
+export { Decoration, DecorationSet, EditorView } from 'prosemirror-view';
 
 export type {
   DecorationAttrs,
@@ -14,3 +15,23 @@ export type {
   EditorProps,
   NodeView,
 } from 'prosemirror-view';
+
+export function parseFromClipboard(
+  view: EditorView,
+  text: string,
+  html: string | null,
+  plainText: boolean,
+  $context: ResolvedPos
+): Slice {
+  return __parseFromClipboard(view, text, html, plainText, $context);
+}
+
+export function serializeForClipboard(
+  view: EditorView,
+  slice: Slice
+): {
+  dom: HTMLDivElement;
+  text: string;
+} {
+  return __serializeForClipboard(view, slice);
+}
