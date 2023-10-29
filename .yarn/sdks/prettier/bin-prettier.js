45b40cfe259c83503419d1213051e60c553a230e
Kushan Joshi <0o3ko0@gmail.com>
2023-10-28 20:56:15 -0400

    yarn 4

--- .yarn/sdks/prettier/bin-prettier.js
+++ .yarn/sdks/prettier/bin-prettier.js
@@ -4,17 +4,17 @@ const {existsSync} = require(`fs`);
 const {createRequire} = require(`module`);
 const {resolve} = require(`path`);
 
-const relPnpApiPath = "../../../../.pnp.cjs";
+const relPnpApiPath = "../../../.pnp.cjs";
 
 const absPnpApiPath = resolve(__dirname, relPnpApiPath);
 const absRequire = createRequire(absPnpApiPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
-    // Setup the environment to be able to require typescript/lib/tsc.js
+    // Setup the environment to be able to require prettier/bin-prettier.js
     require(absPnpApiPath).setup();
   }
 }
 
-// Defer to the real typescript/lib/tsc.js your application uses
-module.exports = absRequire(`typescript/lib/tsc.js`);
+// Defer to the real prettier/bin-prettier.js your application uses
+module.exports = absRequire(`prettier/bin-prettier.js`);
